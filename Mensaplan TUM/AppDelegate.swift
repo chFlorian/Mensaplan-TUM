e6b93c2b8dfb9f1d644c44236f41535b3ac6be29
Florian Schweizer <post.an.flo@gmail.com>
2016-12-13 14:38:42 +0100

    Initial Commit

---
+++ Mensaplan TUM/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Mensaplan TUM
+//
+//  Created by florian schweizer on 13.12.16.
+//  Copyright © 2016 florian schweizer. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Mensaplan TUM/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Mensaplan TUM
+//
+//  Created by florian schweizer on 13.12.16.
+//  Copyright © 2016 florian schweizer. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
